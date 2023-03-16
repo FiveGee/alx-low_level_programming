d977a35f6736515d2c17225ab5c07984e4f5c358
FiveGee <njabulomanda@gmail.com>
2023-03-16 13:55:06 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <stdint.h>
 
 /**
  * main - Entry point
  *
- * Description: Prints the size of various types on the computer it is
- *              compiled and run on.
+ * Description: Prints the size of various types on the computer it is compiled
+ *              and run on.
  *
  * Return: Always 0 (Success)
  */
@@ -16,13 +15,19 @@ printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
+#ifdef __LP64__
 printf("Size of a double: %zu byte(s)\n", sizeof(double));
 printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
+#else
+printf("Size of a double: %zu byte(s)\n", sizeof(double));
+printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
+#endif
 printf("Size of a uint32_t: %zu byte(s)\n", sizeof(uint32_t));
 printf("Size of a uint64_t: %zu byte(s)\n", sizeof(uint64_t));
 
 return (0);
 }
+
 
 
 

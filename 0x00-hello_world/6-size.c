0f0dc93a9435795f440d90537c4f21fed4a4825a
FiveGee <njabulomanda@gmail.com>
2023-03-16 13:37:41 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
+#include <stdint.h>
 
 /**
  * main - Entry point
  *
- * Description: Prints the size of various types on the computer it is compiled
- *              and run on.
+ * Description: Prints the size of various types on the computer it is
+ *              compiled and run on.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
-printf("Size of a double: %d byte(s)\n", sizeof(double));
-printf("Size of a long double: %d byte(s)\n", sizeof(long double));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+printf("Size of a double: %zu byte(s)\n", sizeof(double));
+printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
+printf("Size of a uint32_t: %zu byte(s)\n", sizeof(uint32_t));
+printf("Size of a uint64_t: %zu byte(s)\n", sizeof(uint64_t));
 
 return (0);
 }
+
 
 

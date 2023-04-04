517fdc0c4ee0a039e6aefccbf619531463d8a0d7
FiveGee <njabulomanda@gmail.com>
2023-04-04 12:49:46 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,8 +2,9 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
- * @s: string to be checked
- * @accept: set of bytes to look for
+ * @s: string to be searched
+ * @accept: set of bytes to be searched for
+ *
  * Return: pointer to the byte in s that matches one of the bytes in accept
  */
 char *_strpbrk(char *s, char *accept)
@@ -17,7 +18,8 @@ char *_strpbrk(char *s, char *accept)
 			if (*s == accept[i])
 				return (s);
 		}
+
 		s++;
 	}
-	return ('\\0');
+	return ('\0');
 }

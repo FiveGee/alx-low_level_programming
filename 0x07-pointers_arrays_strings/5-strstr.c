ac08d77f42c4e708d32374df7a2acd7dad406d92
FiveGee <njabulomanda@gmail.com>
2023-04-04 14:22:39 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,21 +12,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i = 0, K = 0;
+	unsigned int i = 0, j = 0;
 
 	while (haystack[i])
 	{
-		while (needle[K] && (haystack[i] == needle[0]))
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			if (haystack[i + K] == needle[K])
-				K++;
+			if (haystack[i + j] == needle[j])
+				j++;
 			else
 				break;
 		}
-		if (needle[K])
+		if (needle[j])
 		{
 			i++;
-			K = 0;
+			j = 0;
 		}
 		else
 			return (haystack + i);

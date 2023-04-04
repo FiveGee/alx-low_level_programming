72aca8ca4f2610101edb8b22d1cca91e2aa18f34
FiveGee <njabulomanda@gmail.com>
2023-04-04 04:50:17 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,23 +9,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j;
+	unsigned int i = 0;
+	int j;
 
-    while (*s)
-    {
-        for (j = 0; accept[j]; j++)
-        {
-            if (*s == accept[j])
-            {
-		i++;
-                break;
-            }
-            if (accept[j + 1] == '\0')
-            return (i);
-        }
-	s++;
-    }
-
-    return (i);
+	while (*s)
+	{
+		for (j = 0; accept[j]; r++)
+		{
+			if (*s == accept[j])
+			{
+				i++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (i);
+		}
+		s++;
+	}
+	return (i);
 }
-

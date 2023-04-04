7fa32f06f54b4807a4a0a35e2787bcaa61fac279
FiveGee <njabulomanda@gmail.com>
2023-04-04 14:32:35 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _strstr - function finds the substring
+ * _strstr - locates a substring
  *
- * @haystack - string to be searched
- * @needle - first occurrence of the substring
+ * @haystack: the longer string to search
+ * @needle: the first occurrence of the substring
  *
- * Return: a pointer to the beginning of the substring, @NULL if not found
+ * Return: a pointer beg of substring or @Null if it not foound.
  */
-
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0, j = 0;

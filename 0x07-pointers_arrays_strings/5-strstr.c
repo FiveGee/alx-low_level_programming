466dad74cb1ec3dd21c0817f5dada26d93a5d255
FiveGee <njabulomanda@gmail.com>
2023-04-04 13:32:23 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,30 +12,30 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+        int i;
 
-	if (*needle == 0)
-		return (haystack);
-	while (*haystack)
-	{
-		i = 0;
+        if (*needle == 0)
+                return (haystack);
+        while (*haystack)
+        {
+                i = 0;
 
-		if (haystack[i] == needle[i])
-		{
-			do 
-			{
-				if (needle[i + 1] == '\0')
-					return (haystack);
+                if (haystack[i] == needle[i])
+                {
+                        do
+                        {
+                                if (needle[i + 1] == '\0')
+                                        return (haystack);
 
-				i++;
+                                i++;
 
-			}
-			while (haystack[i] == needle[i]);
-		}
+                        }
+                        while (haystack[i] == needle[i]);
+                }
 
-		haystack++;
+                haystack++;
 
-	}
+        }
 
-	return ('\0');
+        return ('\0');
 }

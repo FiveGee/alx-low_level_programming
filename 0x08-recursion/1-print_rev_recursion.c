4d6fa672f370b7e0fa9c3906a47281ab1e35bcc5
FiveGee <njabulomanda@gmail.com>
2023-04-05 15:00:17 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,20 +4,18 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: the string to print
+ *
  * Return: void
  */
 
-
-void _print_rev_recursion(char *s) 
+void _print_rev_recursion(char *s)
 {
+if (*s == '\0')
+{
+return;
+}
 
-  if (*s == '\0') 
-  {
-
-    return;
-
-  }
-  _print_rev_recursion(s + 1);
-  putchar(*s);
-
+_print_rev_recursion(s + 1);
+putchar(*s);
+}
 }

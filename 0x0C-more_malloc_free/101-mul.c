5e37cd95115ccdb28a7f2afabd384fa584837280
FiveGee <njabulomanda@gmail.com>
2023-04-13 13:38:36 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,7 +29,7 @@ void _print(char *str, int l)
  * mul - multiplies a char with a string and places the answer into dest
  * @n: char to multiply
  * @num: string to multiply
- * @num_index: last non-NULL index of num
+ * @num_index: last non NULL index of num
  * @dest: destination of multiplication
  * @dest_index: highest index to start addition
  *
@@ -84,8 +84,8 @@ int check_for_digits(char **av)
 
 /**
  * init - initializes a string
- * @str: string to initialize
- * @l: length of string
+ * @str: sting to initialize
+ * @l: length of strinf
  *
  * Return: void
  */
@@ -105,6 +105,7 @@ void init(char *str, int l)
  *
  * Return: zero, or exit status of 98 if failure
  */
+
 int main(int argc, char *argv[])
 {
 	int l1, l2, ln, ti, i;
@@ -118,5 +119,31 @@ int main(int argc, char *argv[])
 			_putchar(e[ti]);
 		exit(98);
 	}
-	for (l1 =
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	ln = l1 + l2 + 1;
+	a = malloc(ln * sizeof(char));
+	if (a == NULL)
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	init(a, ln - 1);
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	{
+		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		if (t == NULL)
+		{
+			for (ti = 0; e[ti]; ti++)
+				_putchar(e[ti]);
+			free(a);
+			exit(98);
+		}
+	}
+	_print(a, ln - 1);
+	return (0);
+}
 
